d88273c0c4bc93eac0691c995420c8f8956fa1e8
markyanushevsky <root@localhost.localdomain>
2016-02-11 14:00:13 -0500

    added comments

--- connector_log.sh
+++ connector_log.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "---------------" >> /data.txt
-date >> /data.txt
+date >> /data.txt # logging time
 echo "---------------" >> /data.txt 
-grep "INFO" /opt/connector/logs/connector.log | head  >> /data.txt 
+grep "INFO" /opt/connector/logs/connector.log | head  >> /data.txt # select first 10 items from connector.log with log level "INFO". Log level can be changed on "DEBUG, WARN, ERROR" 
