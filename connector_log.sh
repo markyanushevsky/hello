92312ca0cf3c1dd972fdac798c3d93b1e55bd505
markyanushevsky <root@localhost.localdomain>
2016-02-12 00:25:30 -0500

    new

--- connector_log.sh
+++ connector_log.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-#echo "---------------" >> /data.txt
-#date >> /data.txt # logging time
-#echo "---------------" >> /data.txt 
-echo "Enter path to log file"
-read path
-echo "Enter log level INFO/ERROR/DEBUG/WARN"
-read log
-echo "Enter limit of lines"
-read lines
-grep "$log" $path | head -$lines # select first 10 items from connector.log with log level "INFO". Log level can be changed on "DEBUG, WARN, ERROR" 
+cat $1 | grep "$2" | head -$3
+
