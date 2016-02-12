825089352f3c46e065eeb5fb975e02b8c25a587b
markyanushevsky <root@localhost.localdomain>
2016-02-11 19:40:44 -0500

    Upgrade

--- connector_log.sh
+++ connector_log.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-echo "---------------" >> /data.txt
-date >> /data.txt # logging time
-echo "---------------" >> /data.txt 
-grep "INFO" /opt/connector/logs/connector.log | head  >> /data.txt # select first 10 items from connector.log with log level "INFO". Log level can be changed on "DEBUG, WARN, ERROR" 
+#echo "---------------" >> /data.txt
+#date >> /data.txt # logging time
+#echo "---------------" >> /data.txt 
+echo "Enter path to log file"
+read path
+echo "Enter log level INFO/ERROR/DEBUG/WARN"
+read log
+echo "Enter limit of lines"
+read lines
+grep "$log" $path | head -$lines # select first 10 items from connector.log with log level "INFO". Log level can be changed on "DEBUG, WARN, ERROR" 
